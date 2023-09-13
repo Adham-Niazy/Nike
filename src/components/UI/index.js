af29a9a6045b4e1b2818f64491c7bda36172b201
Adham-Niazy <adhamniazy@gmail.com>
2023-09-13 06:58:24 +0300

    Service Section And component Card

--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -1,3 +1,4 @@
 export { default as Button } from './Button.jsx';
 export { default as ShoeCard } from './ShoeCard.jsx';
 export { default as PopularProductCard } from './PopularProductCard.jsx';
+export { default as ServiceCard } from './ServiceCard.jsx';

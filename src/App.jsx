34a4bb2b078911278f91ec15c0ee61fbc039ac46
Adham-Niazy <adhamniazy@gmail.com>
2023-09-13 06:48:03 +0300

    Fix App component issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,44 @@
-import { Navbar, Footer } from './components/layout';
+import { Navbar, Footer } from "./components/layout";
 import {
+  CustomerReviews,
   Hero,
   PopularProducts,
-  SuperQuality,
   Services,
-  CustomerReviews,
   SpecialOffer,
-  Subscribe
-} from './components/sections';
+  Subscribe,
+  SuperQuality,
+} from "./components/sections";
 
-const App = () => (
-  <main className="relative">
-    <Navbar />
-    <section className="xl:padding-1 wide:padding-r padding-b">
-      <Hero />
-    </section>
-    <section className="padding">
-      <PopularProducts />
-    </section>
-    <section className="padding">
-      <SuperQuality />
-    </section>
-    <section className="padding-x py-10">
-      <Services />
-    </section>
-    <section className="padding">
-      <SpecialOffer />
-    </section>
-    <section className="bg-pale-blue padding">
-      <CustomerReviews />
-    </section>
-    <section className="padding-x sm:py-32 py-16 w-full">
-      <Subscribe />
-    </section>
-    <section className="bg-black padding-x padding-t pb-8">
-      <Footer />
-    </section>
-  </main>
-);
+const App = () => {
+  return (
+    <main className='relative'>
+      <Navbar />
+      <section className='xl:padding-l wide:padding-r padding-b'>
+        <Hero />
+      </section>
+      <section className='padding'>
+        <PopularProducts />
+      </section>
+      <section className='padding'>
+        <SuperQuality />
+      </section>
+      <section className='padding-x py-10'>
+        <Services />
+      </section>
+      <section className='padding'>
+        <SpecialOffer />
+      </section>
+      <section className='bg-pale-blue padding'>
+        <CustomerReviews />
+      </section>
+      <section className='padding-x sm:py-32 py-16 w-full'>
+        <Subscribe />
+      </section>
+      <section className=' bg-black padding-x padding-t pb-8'>
+        <Footer />
+      </section>
+    </main>
+  );
+};
 
 export default App;

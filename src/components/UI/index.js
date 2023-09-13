b66e997f4b956d9bd0035305e7c1e99597827c39
Adham-Niazy <adhamniazy@gmail.com>
2023-09-13 07:01:14 +0300

    Customer Reviews section and Review Card reusable component

--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -2,3 +2,4 @@ export { default as Button } from './Button.jsx';
 export { default as ShoeCard } from './ShoeCard.jsx';
 export { default as PopularProductCard } from './PopularProductCard.jsx';
 export { default as ServiceCard } from './ServiceCard.jsx';
+export { default as ReviewCard } from './ReviewCard.jsx';
